b99e778097a2a3740aa0e011a8376112803628b9
naokirin <naoki.rin186@gmail.com>
2012-04-29 09:52:47 +0900

    caramel.mliを削除した

--- lib/caramel.mli
+++
@@ -1,4 +0,0 @@
-include module type of Caramel_either
-include module type of Caramel_list
-
-val (@): 'a list -> 'a list -> 'a list
--- lib/caramel_either.mli
+++ lib/caramel_either.mli
@@ -1,5 +1,5 @@
 type ('a, 'b) either = Left of 'a | Right of 'b
 
 module Either : sig
-  val ret_either: ('a -> 'b) -> 'a -> ('c, 'b) either
+  val ret_either: ('a -> 'b) -> 'a -> (exn, 'b) either
 end

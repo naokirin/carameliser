dfd59f6f2fe40e350dea64bf973b872495e2f99d
naokirin <naoki.rin186@gmail.com>
2012-05-03 15:02:42 +0900

    2引数を受け取るコンストラクタのモナドを実装した
    モジュールMonadにMake2を実装した

--- lib/caramel_monad.ml
+++ lib/caramel_monad.ml
@@ -43,4 +43,50 @@ module Monad = struct
     let (>>=) = bind
     let (>>|) x f = map ~f x
   end
+
+  (** 2 argument constructor *)
+  module type Infix2 = sig
+    type ('a, 'b) t
+
+    val (>>=): ('a, 'd) t -> ('d -> ('a, 'b) t) -> ('a, 'b) t
+    val (>>|): ('a, 'd) t -> ('d -> 'b) -> ('a, 'b) t
+  end
+
+  (** Minimam Monad Type *)
+  module type T2 = sig
+    type ('a, 'b) t
+
+    val bind: ('a, 'd) t -> ('d -> ('a, 'b) t) -> ('a, 'b) t
+    val return: 'a -> (_, 'a) t
+  end
+
+  module type S2 = sig
+    type ('a, 'b) t
+
+    include Infix2 with type ('a, 'b) t := ('a, 'b) t
+    include T2 with type ('a, 'b) t := ('a, 'b) t
+
+    val map: f:('d -> 'b) -> ('a, 'd) t -> ('a, 'b) t
+    val join: ('a, ('a, 'b) t) t -> ('a, 'b) t
+    val ignore: ('a, 'b) t -> ('a, unit) t
+    val sequence : ('a, 'b) t list -> ('a, 'b list) t
+  end
+
+  module Make2(M:T2) : S2 with type ('a, 'b) t := ('a, 'b) M.t = struct
+    include M
+
+    let bind = M.bind
+    let return = M.return
+    let map ~f x = bind x (fun n -> return (f n))
+    let join m = bind m (fun x -> x)
+    let ignore t = map t ~f:(fun _ -> ())
+    let rec sequence = function
+      |[] -> return []
+      |x::xs ->
+        bind x (fun x ->
+          (bind (sequence xs) (fun xs ->
+            return (x::xs))))
+    let (>>=) = bind
+    let (>>|) x f = map ~f x
+  end
 end

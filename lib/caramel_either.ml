62f092e89ba9f884cb7bf61fefc78dbf0b635276
naokirin <naoki.rin186@gmail.com>
2012-05-03 16:44:52 +0900

    EitherMonadを実装した
    モジュールEither.EitherMonadを実装した

--- lib/caramel_either.ml
+++ lib/caramel_either.ml
@@ -8,4 +8,14 @@ module Either = struct
       Right (f x)
     with e ->
       Left e
+
+  module EitherMonad = Caramel_monad.Monad.Make2(struct
+    type ('a, 'b) t = ('a, 'b) either
+
+    let bind m f = match m with
+      | Right x -> f x
+      | Left x -> Left x
+
+    let return x = Right x
+  end)
 end
--- lib/caramel_either.mli
+++ lib/caramel_either.mli
@@ -2,4 +2,6 @@ type ('a, 'b) either = Left of 'a | Right of 'b
 
 module Either : sig
   val ret_either: f:('a -> 'b) -> 'a -> (exn, 'b) either
+
+  module EitherMonad : Caramel_monad.Monad.S2 with type ('a, 'b) t := ('a, 'b) either
 end

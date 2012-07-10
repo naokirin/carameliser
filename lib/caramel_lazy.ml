7517ee79aa76a91415f76c731663578765f9a1a2
naokirin <naoki.rin186@gmail.com>
2012-07-10 17:18:26 +0900

    モジュールLazyに標準ライブラリのLazyの関数を追加した

--- lib/caramel_lazy.ml
+++ lib/caramel_lazy.ml
@@ -1,5 +1,14 @@
+type 'a t = 'a lazy_t
+
 module LazyMonad = Caramel_monad.Make(struct
-  type 'a t = 'a Lazy.t
+  type 'a t = 'a t
   let return x = lazy x
   let bind m f = f (Lazy.force m)
 end)
+
+exception Undefined
+let force = Lazy.force
+let force_val = Lazy.force_val
+let lazy_from_fun = Lazy.lazy_from_fun
+let lazy_from_val Lazy.lazy_from_val
+let lazy_is_val = Lazy.lazy_is_val
--- lib/caramel_lazy.mli
+++ lib/caramel_lazy.mli
@@ -1,1 +1,10 @@
-module LazyMonad : Caramel_monad.S with type 'a t := 'a Lazy.t
+type 'a t = 'a lazy_t
+
+module LazyMonad : Caramel_monad.S with type 'a t := 'a t
+
+exception Undefined
+val force : 'a t -> 'a
+val force_val : 'a t -> 'a
+val lazy_from_fun : (unit -> 'a) -> 'a t
+val lazy_from_val : 'a -> 'a t
+val lazy_is_val : 'a t -> bool

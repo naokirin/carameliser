bd8410046282e4ac291245e9d03860dec4923348
naokirin <naoki.rin186@gmail.com>
2012-07-10 17:12:15 +0900

    Stringモジュールの修正
    型をString.tで統一した

--- lib/caramel_string.ml
+++ lib/caramel_string.ml
@@ -1,9 +1,33 @@
-(*module T = struct
-  type t = string
-end
+type t = string
 
-include T*)
-include StringLabels
+let length = StringLabels.length
+let get = StringLabels.get
+let set = StringLabels.set
+let create = StringLabels.create
+let make = StringLabels.make
+let copy = StringLabels.copy
+let sub = StringLabels.sub
+let fill = StringLabels.fill
+let blit = StringLabels.blit
+let concat = StringLabels.concat
+let iter = StringLabels.iter
+let escaped = StringLabels.escaped
+let index = StringLabels.index
+let rindex = StringLabels.rindex
+let index_from = StringLabels.index_from
+let rindex_from = StringLabels.rindex_from
+let contains = StringLabels.contains
+let contains_from = StringLabels.contains_from
+let rcontains_from = StringLabels.rcontains_from
+let uppercase = StringLabels.uppercase
+let lowercase = StringLabels.lowercase
+let capitalize = StringLabels.capitalize
+let uncapitalize = StringLabels.uncapitalize
+let compare = StringLabels.compare
+let unsafe_get = StringLabels.unsafe_get
+let unsafe_set = StringLabels.unsafe_set
+let unsafe_blit = StringLabels.unsafe_blit
+let unsafe_fill = StringLabels.unsafe_fill
 
 let is_empty = function "" -> true | _ -> false
 
@@ -12,4 +36,3 @@ let hd str = if is_empty str then raise (Failure "hd") else get str 0
 let tl str = if is_empty str then raise (Failure "tl") else sub ~pos:1 ~len:((length str) -1) str
 
 let string_of_char c = make 1 c
-
--- lib/caramel_string.mli
+++ lib/caramel_string.mli
@@ -1,11 +1,44 @@
 type t = string
 
-include module type of StringLabels with type t := t
+(** These functions in StringLabels *)
+val length : t -> int
+val get : t -> int -> char
+val set : t -> int -> char -> unit
+val create : int -> t
+val make : int -> char -> t
+val copy : t -> t
+val sub : string -> pos:int -> len:int -> string
+val fill : string -> pos:int -> len:int -> char -> unit
+val blit : src:string -> src_pos:int -> dst:string -> dst_pos:int -> len:int -> unit
+val concat : sep:string -> string list -> string
+val iter : f:(char -> unit) -> string -> unit
+val escaped : t -> t
+val index : t -> char -> int
+val rindex : t -> char -> int
+val index_from : t -> int -> char -> int
+val rindex_from : t -> int -> char -> int
+val contains : t -> char -> bool
+val contains_from : t -> int -> char -> bool
+val rcontains_from : t -> int -> char -> bool
+val uppercase : t -> t
+val lowercase : t -> t
+val capitalize : t -> t
+val uncapitalize : t -> t
+val compare : t -> t -> int
+val unsafe_get : t -> int -> char
+val unsafe_set : t -> int -> char -> unit
+val unsafe_blit : src:string -> src_pos:int -> dst:string -> dst_pos:int -> len:int -> unit
+val unsafe_fill : string -> pos:int -> len:int -> char -> unit
 
+
+(** [is_empty] returns true if string length is zero. *)
 val is_empty : t -> bool
 
+(** [hd] returns first charactor in string. *)
 val hd : t -> char
 
+(** [tl] returns string without a first charactor. *)
 val tl : t -> t
 
+(** [string_of_char] is char to string. *)
 val string_of_char : char -> t
